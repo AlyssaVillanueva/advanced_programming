d33e4454a9bc7d1563287e925a25e1ac1c223708
AlyssaVillanueva <alyvillanueva14@gmail.com>
2020-11-03 12:51:36 +0800

    Added index

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-  
-  echo "Hello world";
-  
+    class User{
+
+        public $name;
+        public $username;
+        public $email;
+
+        function addFriend($name){
+            echo $name. " added a friend.";
+        }
+        function postStatus($name){
+            echo $name. " posted a friend.";
+        }
+    }
+        $example = new User();
+        echo $example->addFriend('Aly');
+        echo "<br>";
+        echo $example->postStatus('Aly');
 ?>
